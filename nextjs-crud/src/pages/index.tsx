01dc4237ca796abcd225ec8bb29a3d5de0edb3f7
Matheus351 <matheusniklaus@gmail.com>
2022-10-26 11:42:50 -0300

    renomeando props e definindo classe Table

--- nextjs-crud/src/pages/index.tsx
+++ nextjs-crud/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Layout from "../components/Layout"
+import Table from "../components/Table"
+import Client from "../core/Client"
 
 export default function Home() {
+   
+  const clients = [
+    new Client('Ana',34,'1'),
+    new Client('Bia',27,'2'),
+    new Client('Carlos',21,'3'),
+    new Client('Pedro',30,'4'),
+  ]
+
   return (
     <div className="flex h-screen justify-center items-center 
     bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout title="Simple Crud">
-        <span>Content</span>
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   )
